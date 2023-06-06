88729b88cc32cf0fd2d8df61b40cf7467ad9cd26
Tim Fan <tim61114@gmail.com>
2023-06-06 00:31:01 -0700

    Revert "Untrack Ctfcseason.php in current branch"
    This reverts commit 788b6d9ca624c7460515425b06936c8aa8ffa91b.

---
+++ application/index/controller/Ctfcseason.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aven
+ * Date: 2018/10/28
+ * Time: 1:30
+ */
+
+namespace app\index\controller;
+
+require_once (__DIR__ . '/../../../thinkphp/library/think/db/Expression.php');
+
+
+use think\Db;
+use think\Session;
+use think\Db\Expression;
+
+class Ctfcseason extends Base
+{
+    const FIELD = 'Name,Date,Venue';
+
+    public function add()
+    {
+        $this->checkauthorization();
+
+        $data = request()->only(self::FIELD, 'post');
+        $this->makeNull($data);
+        // $validator = validate('Ctfc_season');
+        // $result = $validator->check($data);
+        // if (!$result){
+        //     $this->affectedRowsResult(0);
+        // }
+        $result = Db::name('ctfc_season')->insert($data);
+        $this->affectedRowsResult($result);
+    }
+
+    public function delete($id){
+        $this->checkauthorization();
+
+        $result = Db::name('ctfc_season')->where('ID', $id)->delete();
+        $this->affectedRowsResult($result);
+    }
+
+    public function read($id){
+
+        $exp = new Expression('field(ID,'.$id.'),StartTime DESC');
+        $result = Db::name('ctfc_season')
+            // ->order($exp)
+            ->select();
+        $result = array_reverse($result);
+
+
+        if($this->jsonRequest()) {
+            $this->dataResult($result[0]);
+        } else if(count($result)>0){
+            $this->headerAndFooter('ctfc');
+
+            $otherseasons = array_slice($result,1);
+
+            $matches = Db::name('ctfc_matchevent')->where('SeasonID', $result[0]['ID'])
+                // ->order('StartTime','desc')
+                ->select();
+
+            $this->view->assign('matches',$matches);
+            $this->view->assign('thisseason',$result[0]);
+            $this->view->assign('otherseasons',$otherseasons);
+            return $this->view->fetch('ctfcseason/read');
+        } else {
+            header("HTTP/1.0 404 Not Found");
+            die;
+        }
+
+    }
+
+    public function lists(){
+        $list = Db::name('ctfc_season')->paginate(input('pagesize'));
+        $this->paginatedResult(
+            $list->total(),
+            $list->listRows(),
+            $list->currentPage(),
+            $list->items()
+        );
+    }
+
+    public function readRecent(){
+
+        $result = Db::name('ctfc_season')
+            // ->order('StartTime desc')
+            ->select();
+
+
+        if($this->jsonRequest()) {
+            $this->dataResult($result[0]);
+        } else if(count($result)>0){
+            $this->headerAndFooter('ctfc');
+
+            $otherseasons = array_slice($result,1);
+
+            $matches = Db::name('ctfc_matchevent')->where('SeasonID', $result[0]['ID'])
+                // ->order('StartTime','desc')
+                ->select();
+
+            $events= Db::name('ctfc_event')->select();
+            $this->view->assign('recenteventid',$events[0]['ID']);
+            $this->view->assign('matches',$matches);
+            $this->view->assign('thisseason',$result[0]);
+            $this->view->assign('otherseasons',$otherseasons);
+            return $this->view->fetch('ctfcseason/read');
+        } else {
+            header("HTTP/1.0 404 Not Found");
+            die;
+        }
+
+    }
+
+    public function update($id){
+        $this->checkauthorization();
+
+        $data = request()->only(self::FIELD, 'post');
+        $this->makeNull($data);
+//        $validator = validate('Ctfc_season');
+//        $result = $validator->check($data);
+//        if (!$result){
+//            $this->result(0);
+//        }
+        $result = Db::name('ctfc_season')->where('ID', $id)->update($data);
+        $this->affectedRowsResult($result);
+    }
+}
