3aa393bb9c64aee32e23472e77a044af384a4516
shuminCA <zhangshumin1207@gmail.com>
2023-07-05 18:53:40 -0700

    Finish the filter

--- application/route.php
+++ application/route.php
@@ -179,7 +179,7 @@
 \think\Route::get('ctfcseason/:seasonid/apply', 'index/ctfcteam/getapply', [], ['seasonid' => '\d+']);
 \think\Route::post('ctfcseason/:id', 'index/ctfcseason/update', [], ['id' => '\d+']);
 \think\Route::post('ctfcseason', 'index/ctfcseason/add');
-\think\Route::get('ctfcseason/filter', 'index/ctfcseason/filter');
+\think\Route::get('ctfcseason/filter/:id', 'index/ctfcseason/filter', [], ['id' => '\d+']);
 \think\Route::delete('ctfcseason/:id', 'index/ctfcseason/delete', [], ['id' => '\d+']);
 \think\Route::get('ctfcseason/:id', 'index/ctfcseason/read', [], ['id' => '\d+']);
 \think\Route::get('ctfcseason', 'index/ctfcseason/lists');
