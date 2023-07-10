a46b7fcec624578948c87c578b433640a2b975e3
shuminCA <zhangshumin1207@gmail.com>
2023-07-10 12:10:32 -0700

    Change '男', '女' to 'M', 'F'; Add 'Mix';

--- application/index/controller/Ctfcseason.php
+++ application/index/controller/Ctfcseason.php
@@ -168,7 +168,7 @@ class Ctfcseason extends Base
                 $filter['ItemName'] = $itemName;
             }
             if (!empty($gender)) {
-                $filter['Gender'] = $gender === '男' ? 0 : 1;
+                $filter['Gender'] = $gender;
             }
             if (!empty($ageGroupName)) {
                 $filter['AgeGroupName'] = $ageGroupName;
