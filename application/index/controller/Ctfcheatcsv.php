a99c25f6419d048fac415054ff2a32d64428a7b0
ericlu <ericlu9256@gmail.com>
2023-07-26 14:14:41 -0700

    modification for column data, database mod required, todo-player names

--- application/index/controller/Ctfcheatcsv.php
+++ application/index/controller/Ctfcheatcsv.php
@@ -8,33 +8,63 @@ use think\Response;
 
 class CtfcheatCSV extends Base
 {
-    const FIELD = 'ID,EventID,HeatID,LaneNumber,TeamName,ItemAgeGroupSex,Player1,Player2,Player3,Player4,Player5,Player6,Result,Note,IsSingle,HeatSize,ItemName,Gender,AgeGroupNumber,ItemPlayerID,TeamID,ItemID';
+    const FIELD = 'EventID,HeatID,LaneNumber,TeamName,TeamAbbr,ItemAgeGroupSex,Player1,Player2,Player3,Player4,Player5,Player6,Result,Note,IsSingle,HeatSize,ItemName,Gender,AgeGroupNumber,ItemPlayerID,TeamID,ItemID,Division';
 
     public function generateCSV()
     {
         // Retrieve data from the database
-        $list = Db::name('ctfc_heat_view')->order(['EventID', 'HeatID', 'LaneNumber'])->select();
+        $list = Db::name('ctfc_heat_view')
+        ->order(['EventID', 'HeatID', 'LaneNumber'])
+        ->select();
 
         // Modify the player1-6 fields to combine them into a single column
         $modifiedList = [];
         foreach ($list as $heateachrow) {
             $newTable = [];
-            $newTable['ID'] = $heateachrow['ID'];
-            $newTable['EventID'] = $heateachrow['EventID'];
-            $newTable['HeatID'] = $heateachrow['HeatID'] ? $heateachrow['HeatID'] : 1;
-            $newTable['LaneNumber'] = $heateachrow['LaneNumber'];
-            $newTable['TeamName'] = $heateachrow['TeamName'];
-            $newTable['ItemAgeGroupSex'] = $heateachrow['ItemAgeGroupSex'];
-
-            $players = [];
-            for ($i = 1; $i <= 6; $i++) {
-                if ($heateachrow["Player{$i}"]) {
-                    $players[] = $heateachrow["Player{$i}"];
-                }
+            $newTable['D ( DO NOT CHANGE)'] ='D';
+            $newTable['Last Name'] ='LN';
+            $newTable['First Name'] ='FN';
+            $newTable['LEAVE BLANK'] ='';
+            // Check if the gender value is 'M' or 'F', otherwise set it to 'M'
+            $gender = $heateachrow['Gender'];
+            $gender = (strtoupper($gender) === 'M' || strtoupper($gender) === 'F') ? strtoupper($gender) : 'M';
+            $newTable['Gender M or F'] =$gender;
+            $newTable['Birthdate'] ='';
+            $newTable['Club(Abbr)'] = $heateachrow['TeamAbbr'];
+            $newTable['Club'] = $heateachrow['TeamName'];
+            $newTable['Age'] ='';
+            $newTable['Grade'] ='';
+            // Strip any characters other than integers and 'x' from the 'Event' field
+            $event = $heateachrow['ItemName'];
+            $strippedEvent = preg_replace('/[^0-9x]/i', '', $event);
+            // Format the EVENT value to align in the cell
+            $formattedEvent = '="' . $strippedEvent . '"';
+            $newTable['EVENT (only 1)'] = $formattedEvent;
+            $newTable['SEED TIME'] ='';
+            $newTable['M ( DO NOT CHANGE)'] ='M';
+            // If gender is not 'M' or 'F', concatenate "mixed" with the 'Division' value
+            $dgender = $heateachrow['Gender'];
+            if ($dgender !== 'M' && $dgender !== 'F') {
+                $newTable['DIVISION'] = trim($heateachrow['Division']) . ' mixed';
+            } else {
+                $newTable['DIVISION'] = trim($heateachrow['Division']);
             }
-            $newTable['Players'] = implode(', ', $players);
-            $newTable['Result'] = $heateachrow['Result'];
-            $newTable['Note'] = $heateachrow['Note'];
+            // $newTable['ID'] = $heateachrow['ID'];
+            // $newTable['EventID'] = $heateachrow['EventID'];
+            // $newTable['HeatID'] = $heateachrow['HeatID'] ? $heateachrow['HeatID'] : 1;
+            // $newTable['LaneNumber'] = $heateachrow['LaneNumber'];
+            // $newTable['ItemAgeGroupSex'] = $heateachrow['ItemAgeGroupSex'];
+
+            // $players = [];
+            // for ($i = 1; $i <= 6; $i++) {
+            //     if ($heateachrow["Player{$i}"]) {
+            //         $players[] = $heateachrow["Player{$i}"];
+            //     }
+            // }
+            // $newTable['Players'] = implode(', ', $players);
+            // $newTable['Result'] = $heateachrow['Result'];
+            // $newTable['Note'] = $heateachrow['Note'];
+            
 
             $modifiedList[] = $newTable;
         }
